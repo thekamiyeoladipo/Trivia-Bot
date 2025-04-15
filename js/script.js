ef21f3bea8a64b44757bccb646b3643eeda889ca
kamiye <kamiye31@gmail.com>
2025-04-15 20:25:33 +0100

    Build Sentence Maker

--- js/script.js
+++ js/script.js
@@ -16,3 +16,35 @@ console.log(codingFact);
 codingFact = 'I love coding in ' + favoriteLanguage + '.';
 console.log(codingFact);
 console.log("It was fun sharing these facts with you. Goodbye! -" + botName + " from " + botLocation + ".");
+
+
+// This section below is to build a sentence maker to avoid building another repo
+
+let adjective = "mighty";
+let noun = "Dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "massive";
+let noun2 = "chickens";
+
+let firstStory = 
+    "Once upon a time, there was a(n) " + adjective + " " + noun + 
+    " who loved to eat " + noun2 + ". " + 
+    "The " + noun + " lived in a " + place + 
+    " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+
+adjective = "brave";
+noun = "Knight";
+verb = "determined";
+place = "castle";
+adjective2 = "shiny";
+noun2 = "swords";
+
+let secondStory = 
+"Once upon a time, there was a(n) " + adjective + " " + noun + 
+" who loved to eat " + noun2 + ". " + 
+"The " + noun + " lived in a " + place + 
+" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);

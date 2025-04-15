965a079ba5fd37749f20a73769e2acbad7ae41a8
kamiye <kamiye31@gmail.com>
2025-04-15 18:44:56 +0100

    trivia bot completed without errors

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('Hello, World!');
+
+let botName = "Chatbot";
+let botLocation = "Earth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =  favoriteLanguage + " is a versatile language.";
+console.log(codingFact);
+
+codingFact = 'One of my best languages to use is' + favoriteLanguage + '.';
+
+console.log(codingFact);
+codingFact = 'I love coding in ' + favoriteLanguage + '.';
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! -" + botName + " from " + botLocation + ".");
